278f122f55d4275de804c946ac915085ea9cd139
rfm-jitin <jitinchekka2@gmail.com>
2023-04-17 10:38:40 +0530

    streamlit update

--- main.py
+++ main.py
@@ -48,7 +48,7 @@ def pull_sheet_data(SCOPES, SPREADSHEET_ID, DATA_TO_PULL):
 import pandas as pd
 
 SCOPES = ["https://www.googleapis.com/auth/spreadsheets"]
-SPREADSHEET_ID = "1SO_75vaxuNKqEC3u1TRsboGCvhix74Fv3SDrmetMJ0k"
+SPREADSHEET_ID = "12IH81jN9DCpk5lgD_dhstWMgndpY92qDusWYTK8Tvas"
 DATA_TO_PULL = "Sheet1"
 data = pull_sheet_data(SCOPES, SPREADSHEET_ID, DATA_TO_PULL)
 df = pd.DataFrame(data)
@@ -57,17 +57,17 @@ df.drop(df.columns[[0, 1]], axis=1, inplace=True)
 df.columns = ["temp", "current", "gas", "class"]
 # For column 'class', replace None with '0'
 df["class"] = df["class"].fillna(0)
-
+print(df)
 # Generate random data for class 1 with temp > 60 and gas > 200
-import random
+# import random
 
-for i in range(0, 30):
-    df.loc[len(df.index)] = [
-        random.randint(60, 100),
-        random.randint(0, 1000),
-        random.randint(200, 500),
-        1,
-    ]
+# for i in range(0, 30):
+#     df.loc[len(df.index)] = [
+#         random.randint(60, 100),
+#         random.randint(0, 1000),
+#         random.randint(200, 500),
+#         1,
+#     ]
 
 # Drop first row
 df.drop(df.index[0], inplace=True)
@@ -85,16 +85,23 @@ gnb = GaussianNB()
 X = df.drop("class", axis=1)
 y = df["class"]
 gnb.fit(X, y)
-X_test = [[30, 5, 100]]
+# Take the last row without class column and convert it to a list
+X_test = df.tail(1).drop("class", axis=1)
+print("X_test: ", X_test)
 y_test = [0]
 y_pred = gnb.predict(X_test)
 print("Predicted class: ", y_pred[0])
 
 import streamlit as st
 
+# Print the dataframe
+st.write(df)
 # Print the predicted class
 if y_pred == 0:
+    # Write the test data
+    st.write("Test data: ", X_test) 
     st.write("Predicted class: safe")
 else:
+    st.write("Test data: ", X_test) 
     st.write("Predicted class: unsafe")
 
